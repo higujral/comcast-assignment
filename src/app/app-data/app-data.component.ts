696ccfd3dcf700db8555135b222d29e96dfa3740
Hitesh Gujral <hitesh.gujral@gmail.com>
2017-12-11 22:55:01 -0800

    added sorting

--- src/app/app-data/app-data.component.ts
+++ src/app/app-data/app-data.component.ts
@@ -7,17 +7,52 @@ import { IAppData } from '../shared/models/IAppData';
   selector: 'app-app-data',
   templateUrl: './app-data.component.html',
   styleUrls: ['./app-data.component.css'],
-  providers: [ AppDataService ]
+  providers: [AppDataService]
 })
 export class AppDataComponent implements OnInit {
 
-  appData: IAppData [];
+  appData: IAppData[];
+  sortedAscending: boolean;
 
   constructor(private appDataService: AppDataService) { }
 
   ngOnInit() {
     this.appDataService.getData().subscribe(data => {
       this.appData = <any>data;
+    });
+  }
+
+  sort(property) {
+    if (!this.sortedAscending) {
+      this.sortAcending(property);
+      this.sortedAscending = true;
+    } else {
+      this.sortDescending(property);
+      this.sortedAscending = false;
+    }
+  }
+
+  sortAcending(property) {
+    this.appData.sort((a, b) => {
+      if (a[property] < b[property]) {
+        return -1;
+      }
+      if (a[property] > b[property]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  sortDescending(property) {
+    this.appData.sort((a, b) => {
+      if (a[property] < b[property]) {
+        return 1;
+      }
+      if (a[property] > b[property]) {
+        return -1;
+      }
+      return 0;
     });
   }
 
